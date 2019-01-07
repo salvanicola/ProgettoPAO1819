391446c8098e05c652257acf264d3d5f1c5b0a7c
salvanicola <nicola.salvadore@gmail.com>
2019-01-07 20:01:33 +0100

    prova commit con QT, aggiunto container con lista

---
+++ QontainerList/containerlist.h
@@ -0,0 +1,42 @@
+#ifndef CONTAINERLIST_H
+#define CONTAINERLIST_H
+
+template<class T>
+class ContainerList
+{
+    friend class iterator;
+private:
+    class nodo{
+    public:
+        T info;
+        nodo* next;
+        nodo();
+        nodo(const T&, nodo*);
+    };
+    nodo* first;
+    nodo* last;
+public:
+    class iterator{
+    private:
+        ContainerList::nodo* p;
+        bool operator==(const iterator&);
+        bool operator!=(const iterator&);
+        iterator& operator++(); //prefisso
+        iterator operator++(int); //postfisso
+    };
+    ContainerList();
+    ContainerList(const T&);
+    ContainerList(const ContainerList&);
+    bool vuota()const;
+    void push_back(const T&);
+    void pop_back();
+    iterator search(const T)const;
+    iterator begin()const;
+    iterator end()const;
+    T& operator[](const iterator&)const;
+    T* extract(const iterator&);
+};
+
+
+
+#endif // CONTAINERLIST_H
